f4c864b7cfa8286b5c3c3041b1e24dc87044e478
autentifica42LMVG <81276567+autentifica42LMVG@users.noreply.github.com>
2021-03-23 21:10:06 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyD0FdRhEEAgN5FmCbAbh9iiNcEkt2dXrx8",
+    authDomain: "autentica42lmvg.firebaseapp.com",
+    projectId: "autentica42lmvg",
+    storageBucket: "autentica42lmvg.appspot.com",
+    messagingSenderId: "931512255900",
+    appId: "1:931512255900:web:2178604e07db9deab3b459",
+    measurementId: "G-ZXGK3RGKV8"
+    });
